c31bc0bf8f24d14254509612f35629fb08e1a1f6
hbagde424 <hbagde424@gmail.com>
2025-08-22 10:52:45 +0530

    aaa

--- Backend/models/RolePermission.js
+++ Backend/models/RolePermission.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const rolePermissionSchema = new mongoose.Schema({
-  role: { type: mongoose.Schema.Types.ObjectId, ref: 'Role', required: true },
-  permission: { type: mongoose.Schema.Types.ObjectId, ref: 'Permission', required: true },
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Role', required: true
+  },
+  permission: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Permission', required: true
+  },
 });
 
 rolePermissionSchema.index({ role: 1, permission: 1 }, { unique: true });
