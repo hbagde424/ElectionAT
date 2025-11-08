8f33e0bc9a7f93f43b9950133ecff8e389482a83
hbagde424 <hbagde424@gmail.com>
2025-11-08 17:44:50 +0530

    Update logo to use LogoRemovebg.png
    Replaced references to logo.png with LogoRemovebg.png in LogoIcon.jsx, LogoMain.jsx, and Header.jsx. Added the new LogoRemovebg.png asset to the project for improved logo appearance.

--- frontend/src/components/logo/LogoMain.jsx
+++ frontend/src/components/logo/LogoMain.jsx
@@ -1,6 +1,6 @@
 // material-ui
 import { useTheme } from '@mui/material/styles';
-import logo from './logo.png';
+import logo from './LogoRemovebg.png';
 
 /**
  * if you want to use image instead of <svg> uncomment following.
